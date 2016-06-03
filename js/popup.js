0fc5c2ffdecaecb569081445567aa18551af7939
zeruitle <zeruitle@gmail.com>
2016-06-02 17:33:58 -0700

    start working on popup.js

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+/*
+ *each domain has its own setting
+ *each domain can have multiple profiles
+ *when init, key:domain, value:profiles
+ *profiles store encryption options
+ */
+function init(){
+    
+}
